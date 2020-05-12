7e98e9e244ab174eace76527b439c93754f75534
samuelshan123 <samuelshan123@gmail.com>
2020-05-12 06:45:51 -0700

    Initial commit

---
+++ app/src/main/java/me/samuel/spinnerjson/Config.java
@@ -0,0 +1,13 @@
+package me.samuel.spinnerjson;
+
+public class Config {
+    //JSON URL
+    public static final String DATA_URL = "https://www.pico.games/publichelper/json/upi.json";
+
+    //Tags used in the JSON String
+    public static final String TAG_USERNAME = "Title";
+    public static final String TAG_NAME = "UPIID";
+
+    //JSON array name
+    public static final String JSON_ARRAY = "Fundlist";
+}
